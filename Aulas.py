7a093607b2146820421f1ea5be08f13d6861e09a
anaclara169 <clara4566l@gmail.com>
2026-07-10 21:36:32 -0300

    Aulas.py

--- Aulas.py
+++ Aulas.py
@@ -1,2 +1,22 @@
-# alp_python_1info_2026
-Repositório da disciplina de Algoritmos e Lógica de Programação do 1 ano info 2026-1 do IFPI P
+soma = 0
+quantidade = 0
+maior = 0
+
+numero = int(input("Digite um número: "))
+
+while numero >= 0:
+    soma = soma + numero
+    quantidade = quantidade + 1
+
+    if quantidade == 1 or numero > maior:
+        maior = numero
+
+    numero = int(input("Digite um número: "))
+
+if quantidade > 0:
+    media = soma / quantidade
+    print("Soma:", soma)
+    print("Média:", media)
+    print("Maior número:", maior)
+else:
+    print("Nenhum número positivo foi informado.")
